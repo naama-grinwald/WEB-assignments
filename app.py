7db9fb50f59e2d398063d2a033e5ddf4f0cca784
naama-grinwald <naama.grinwald@gmail.com>
2021-12-09 21:26:06 +0200

    app assignment 7

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
+
+# Make one usage of redirect() function and one usage of redirect() and url_for() functions.
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home_page():
+    return 'This is Home Page!'
 
+@app.route('/about')
+def about_func():
+    return 'About me page!'
+
+@app.route('/hello')
+def hello_world():
+    return redirect('/home')
+
+@app.route('/home')
+def home_func():
+    return redirect(url_for('home_page'))
+
+@app.route('/catalog')
+def catalog_func():
+    return "welcome to catalog page"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

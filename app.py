4d43ecb3be2da4fe406664a5be9c6ca379e86ace
naama-grinwald <naama.grinwald@gmail.com>
2022-01-21 20:37:27 +0200

    assignment 12

--- app.py
+++ app.py
@@ -114,5 +114,25 @@ def req_backend_func():
     return render_template('assignment11.html', user=res)
 
 
+@app.route('/assignment12/restapi_users', defaults={'user_id': 1})
+@app.route('/assignment12/restapi_users/<int:user_id>')
+def assignment12(user_id):
+    query = 'select * from users where id=%s' % user_id
+    users = interact_db(query=query, query_type='fetch')
+    if len(users) == 0:
+        return_dict = {
+            'status': 'failed',
+            'message': 'user not found'
+        }
+    else:
+        return_dict = {
+            'status': 'success',
+            f'id': users[0].id,
+            'name': users[0].name,
+            'email': users[0].id,
+        }
+    return jsonify(return_dict)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
